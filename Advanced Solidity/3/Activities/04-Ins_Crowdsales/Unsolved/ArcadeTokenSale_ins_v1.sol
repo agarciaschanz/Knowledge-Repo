405617fc6da7a7d444162632cedca8be90aa1ddb
Andrew Garcia-Schanz <andrew.garciaschanz@gmail.com>
2023-05-02 17:32:13 -0500

    Add files via upload

---
+++ Advanced Solidity/3/Activities/04-Ins_Crowdsales/Unsolved/ArcadeTokenSale_ins_v1.sol
@@ -0,0 +1,5 @@
+pragma solidity ^0.5.0;
+
+import "./ArcadeTokenMintable_ins_v2.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
---
+++ Advanced Solidity/3/Activities/05-Stu_ArcadeTokenSale/Unsolved/ArcadeTokenSale_stu_v1.sol
@@ -0,0 +1,34 @@
+pragma solidity ^0.5.0;
+
+// @TODO: Import ArcadeTokenMintable from ./ArcadeTokenMintable_stu_v2.sol
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
+
+contract ArcadeTokenSale is Crowdsale, MintedCrowdsale {
+
+    // @TODO: Build the constructor, passing in the parameters that Crowdsale needs
+
+}
+
+contract ArcadeTokenSaleDeployer {
+
+    // @TODO: Add public addresses to keep track of the token_address and arcade_sale_address
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        address payable wallet // this address will receive all Ether raised by the sale
+    )
+        public
+    {
+        // @TODO: create the ArcadeToken and keep its address handy
+        // Your code here!
+
+        // @TODO: create the ArcadeTokenSale and tell it about the token, then keep its address handy
+        // Your code here!
+
+        // @TODO: make the ArcadeTokenSale contract a minter, then have the ArcadeTokenSaleDeployer renounce its minter role
+        // Your code here!
+    }
+}
